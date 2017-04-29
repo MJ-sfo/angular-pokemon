cf5e7fd4bbc8b43b7c9994fe0e906e5cd1888ad5
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-28 18:02:03 -0700

    cleaner PokiIndexController.js

--- public/scripts/controllers/PokemonsIndexController.js
+++ public/scripts/controllers/PokemonsIndexController.js
@@ -8,21 +8,18 @@ function pokemonsShowController ($http, $routeParams) {
 // function pokemonsShowController($http, $routeParams, $location) {
   var vm = this;
   var pokemonId = $routeParams.id;
-  // var pokemonId = $route.current.params;
   console.log("pokemonId is : ", pokemonId);
-// console.log("vm on indiv page is: ", vm);
-// console.log(vm.poki.pokemons);
+
   $http({
     method: 'GET',
     url: 'https://super-crud.herokuapp.com/pokemon/'+ pokemonId
 
   }).then(function successCallback(response, onError) {
   // vm.poki = response.data;
-  vm.test = "sanity check for vm";
-  console.log('response is: ', response);
-    // vm.pokemons = response.data;  // responsee is object/data/array of pokemons/object of individual pokemons
-    console.log("response to GET in pokemonsShowController");
-    console.log('here\'s the data for pokemon', pokemonId, ':', response.data);
+  // vm.test = "sanity check for vm";
+  // console.log('response is: ', response);
+    vm.onepoki = response.data;
+    console.log('here\'s the data for pokemon', pokemonId, ':', vm.onepoki);
   }, function errorCallback(onError) {
     console.log('There was an error getting the data', onError);
   });
